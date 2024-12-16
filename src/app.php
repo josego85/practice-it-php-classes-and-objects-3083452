0fb409f921ce852728c807b21dffd348c3cdd341
josego <josego85@gmail.com>
2024-12-16 17:50:03 +0100

    feat: add challenge 3

--- src/app.php
+++ src/app.php
@@ -3,5 +3,18 @@
 include '_includes.php';
 
 $admin = new Member();
+$admin->username = 'admin';
+$admin->role = 'admin';
 
 $member = new Member();
+$member->username = 'member';
+
+$workspace = $admin->createWorkspace("domain.com");
+$admin->addWorkspaceMember($member, $workspace);
+
+$chat = $member->createChat("Test1", $workspace);
+$member->postMessageToChat("Hello", $chat);
+
+echonl("URL: " . $workspace->getUrl());
+echonl($workspace->members);
+echonl($chat->messages);
