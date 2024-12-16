17b3fab502ec3684e7671ff73e06986facc05336
josego <josego85@gmail.com>
2024-12-16 19:24:27 +0100

    fix: challenge 5

--- src/app.php
+++ src/app.php
@@ -13,3 +13,15 @@ $admin->addWorkspaceMember($memberB, $workspace);
 
 // creating a chat using the Chat class
 // $chat = $memberA->createChat('general', $workspace);
+
+$channel = $admin->createChannel('Channel1', $workspace);
+$admin->postMessageToChat('Hello', $channel);
+$directMessage = $memberA->createDirectMessage([$memberB], $workspace);
+$memberB->postMessageToChat('Hello', $directMessage);
+
+// echonl($channel, true);
+// echonl(PHP_EOL);
+// echonl($directMessage, true);
+
+echonl($channel->getMessages());
+echonl($directMessage->getMessages($memberB));
