ffde75ca1181632419a7572719186e51b7473da3
josego <josego85@gmail.com>
2024-12-16 16:22:02 +0100

    cpmplete challege 2

--- src/app.php
+++ src/app.php
@@ -1,3 +1,16 @@
 <?php
 
 include '_includes.php';
+
+$chat = new Chat();
+$member = new Member();
+$message = new Message();
+$workspace = new Workspace();
+
+$flag = false;
+echonl($chat, $flag);
+echonl($member, $flag);
+echonl($message, $flag);
+echonl($workspace, $flag);
+
+print_r(get_declared_classes());
